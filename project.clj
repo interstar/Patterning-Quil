468dc2b43bfa156303b8f0d5a3c2ef23d046d242
phil jones <interstar@gmail.com>
2014-12-03 02:44:07 -0200

    init

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject com.alchemyislands/patterning_quil "0.1.0-SNAPSHOT"
+  :description "Quil Wrapper for Patterning library"
+  :url "http://alchemyislands.com"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :dependencies [[org.clojure/clojure "1.5.1"]
+			   [org.clojure/math.numeric-tower "0.0.4"]
+                           [quil "1.7.0"]
+                           [com.alchemyislands/patterning "0.3.0-SNAPSHOT"]]
+
+  :aot [patterning_quil.core]
+  :main patterning_quil.core
+  )
